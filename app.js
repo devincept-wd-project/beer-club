bb8d4f3055cb565b9d75e17c16436b1d3186e374
pawan deore <pavandeore90@gmail.com>
2021-03-27 10:30:09 +0530

    instructions added

---
+++ app.js
@@ -0,0 +1,13 @@
+//  API URL https://api.punkapi.com/v2/beers
+
+// You will make GET Request to this URL using FETCH and get data then showcase it on the Webpage thats it
+
+// Terms to Search on Google
+
+// How to make GET request using FETCH 
+// how to use FETCH in JavaScript
+
+
+// If you got stuck 
+// i am here to help just post on slack channel
+// good luck team
